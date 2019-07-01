c9fe19be74141a19f7729e166506dc7afbcf910a
lewistipping <42076180+lewistipping@users.noreply.github.com>
2019-07-01 23:33:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,129 +1,85 @@
-var options = ["Chinese", "Chippy","Pizza"];
-
-var startAngle = 0;
-var arc = Math.PI / (options.length / 2);
-var spinTimeout = null;
-
-var spinArcStart = 10;
-var spinTime = 0;
-var spinTimeTotal = 0;
-
-var ctx;
-
-document.getElementById("spin").addEventListener("click", spin);
-
-function byte2Hex(n) {
-  var nybHexString = "0123456789ABCDEF";
-  return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
-}
-
-function RGB2Color(r,g,b) {
-	return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
-}
-
-function getColor(item, maxitem) {
-  var phase = 0;
-  var center = 128;
-  var width = 127;
-  var frequency = Math.PI*2/maxitem;
+$( document ).ready(function() {
+    LogWithoutTimestamp("Hello!<br/> Welcome to my site");
   
-  red   = Math.sin(frequency*item+2+phase) * width + center;
-  green = Math.sin(frequency*item+0+phase) * width + center;
-  blue  = Math.sin(frequency*item+4+phase) * width + center;
-  
-  return RGB2Color(red,green,blue);
+});
+
+function getCurrentTime() {
+  var date = new Date();
+  var hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
+  var minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  var seconds = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
+  var time = "[" + hours + ":" + minutes + ":" + seconds + "]";
+  return time;
 }
 
-function drawRouletteWheel() {
-  var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    var outsideRadius = 200;
-    var textRadius = 160;
-    var insideRadius = 125;
+setInterval(function() {
+  $(".current-timestamp").text(getCurrentTime());
+  //$("#cmdline").focus();
+}, 500);
 
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0,0,500,500);
+$(document).on("keypress", function(e) {
+  if (e.which == 13) {
+    CommandEntered($("#cmdline").text());
+    $("#cmdline").text("");
+    $("#cmdline").focus();
+  }
+  /*if(e.which == 38) 
+    {
+      $('#cmdline').text(lastcommand);
+    }*/
+  //Add previous command functionality
+});
 
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-
-    ctx.font = 'bold 12px Helvetica, Arial';
-
-    for(var i = 0; i < options.length; i++) {
-      var angle = startAngle + i * arc;
-      //ctx.fillStyle = colors[i];
-      ctx.fillStyle = getColor(i, options.length);
-
-      ctx.beginPath();
-      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
-      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-      ctx.stroke();
-      ctx.fill();
-
-      ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur    = 0;
-      ctx.shadowColor   = "rgb(220,220,220)";
-      ctx.fillStyle = "black";
-      ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius, 
-                    250 + Math.sin(angle + arc / 2) * textRadius);
-      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-      var text = options[i];
-      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-      ctx.restore();
-    } 
-
-    //Arrow
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
-    ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.fill();
+function LogWithoutTimestamp(ToBeLogged)
+{
+  $(".previous-messages").append("<div class='d-block'><div class='timestamp d-inline-block'>" +
+      "</div>" +
+      "<div class='spacer d-inline-block'>></div><div class='command d-inline-block'>" +
+      ToBeLogged +
+      "</div></div>")
+}
+function LogWithTimestamp(ToBeLogged) {
+  $(".previous-messages").append(
+    "<div class='d-block'><div class='timestamp d-inline-block'>" +
+      getCurrentTime() +
+      "</div>" +
+      "<div class='spacer d-inline-block'>></div><div class='command d-inline-block'>" +
+      ToBeLogged +
+      "</div></div>"
+  );
+}
+function CommandEntered(command) {
+  command = command.toLowerCase();
+  switch (command) {
+    case "help":
+      LogWithTimestamp("help - Display Commands");
+      LogWithTimestamp("clear - Clear messages from previous commands");
+      break;
+    case "clear":
+      $(".previous-messages").empty();
+      //$(".message").remove();
+      break;
+    case "about":
+      LogWithTimestamp("This site was created by Lewis Tipping");
+      break;
+    case "":
+      break;
+    case "meh":
+      var win = window.open("https://youtu.be/yZt6eP7cmHM", "_blank");
+      if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+      } else {
+        //Browser has blocked it
+        alert("Please allow popups for this website");
+      }
+      break;
+      case "quail":
+      $(".audio")[0].play();
+      LogWithTimestamp("Just Right");
+      break;
+    default:
+      LogWithTimestamp(command + " is not recognized as a command");
+      break;
   }
 }
-
-function spin() {
-  spinAngleStart = Math.random() * 10 + 10;
-  spinTime = 0;
-  spinTimeTotal = Math.random() * 3 + 4 * 1000;
-  rotateWheel();
-}
-
-function rotateWheel() {
-  spinTime += 30;
-  if(spinTime >= spinTimeTotal) {
-    stopRotateWheel();
-    return;
-  }
-  var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-  startAngle += (spinAngle * Math.PI / 180);
-  drawRouletteWheel();
-  spinTimeout = setTimeout('rotateWheel()', 30);
-}
-
-function stopRotateWheel() {
-  clearTimeout(spinTimeout);
-  var degrees = startAngle * 180 / Math.PI + 90;
-  var arcd = arc * 180 / Math.PI;
-  var index = Math.floor((360 - degrees % 360) / arcd);
-  ctx.save();
-  ctx.font = 'bold 30px Helvetica, Arial';
-  var text = options[index]
-  ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
-  ctx.restore();
-}
-
-function easeOut(t, b, c, d) {
-  var ts = (t/=d)*t;
-  var tc = ts*t;
-  return b+c*(tc + -3*ts + 3*t);
-}
-
-drawRouletteWheel();
